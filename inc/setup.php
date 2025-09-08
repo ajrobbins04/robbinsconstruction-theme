8287139121f593bc09597d715ec1870942eb510a
ajrobbins44 <rob16003@byui.edu>
2025-09-08 14:40:04 -0700

    additional theme support added

--- inc/setup.php
+++ inc/setup.php
@@ -5,6 +5,25 @@
 function robbins_setup() {
     add_theme_support("title-tag");
     add_theme_support("post-thumbnails");
+    add_theme_support("html5", [
+        "search-form",
+        "comment-form",
+        "comment-list",
+        "gallery",
+        "caption",
+    ]);
+    add_theme_support("custom-logo", [
+        "height"      => 80,
+        "width"       => 240,
+        "flex-height" => true,
+        "flex-width"  => true,
+    ]);
+
+    add_theme_support("align-wide");
+    add_theme_support("wp-block-styles");
+    add_theme_support("editor-styles");
+    add_editor_style("dist/style.css");
+    
     register_nav_menus([
         "primary" => __("Primary Menu", "robbins"),
     ]);
